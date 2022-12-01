4fbfa27186e59396191ad83ce3a253501dbe11fe
dyingvoid <dyingvoid@gmail.com>
2022-12-01 06:57:29 +0500

    Pre final

--- Series.cs
+++
@@ -1,8 +0,0 @@
-namespace DBFirstLab;
-
-public class Series<T> where T : struct
-{
-    public String Name { get; set; }
-    public List<T> Data { get; set; }
-    public Type Type => typeof(T);
-}
